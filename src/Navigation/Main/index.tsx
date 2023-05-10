0a89aa869f5fb7790ce7fafbbcaef2bb3aebb5c4
lehuyngo <80335594+lehuyngo@users.noreply.github.com>
2023-05-10 19:17:59 +0700

    Add navigation bottom

--- src/Navigation/Main/index.tsx
+++ src/Navigation/Main/index.tsx
@@ -1,19 +1,54 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeContainer } from "@/Screens/Home";
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const Tab = createBottomTabNavigator();
 
 // @refresh reset
 export const MainNavigator = () => {
   return (
     <Tab.Navigator>
+      
+      
       <Tab.Screen
-        name="Home"
+        name="Feed"
         component={HomeContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+          tabBarLabel: 'Tra cứu',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="map-outline" color={color} size={size} />
+          ),
+          
+        }}
+      />
+      <Tab.Screen
+        name="Vé của tôi"
+        component={HomeContainer}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="ticket-confirmation-outline" color={color} size={size} />
+          ),
+          
+        }}
+      />
+      <Tab.Screen
+        name="Trang của tôi"
+        component={HomeContainer}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+         
+        }}
+      />
+      <Tab.Screen
+        name="Cài đặt"
+        component={HomeContainer}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cog-outline" color={color} size={size} />
+          ),
+          
         }}
       />
     </Tab.Navigator>
