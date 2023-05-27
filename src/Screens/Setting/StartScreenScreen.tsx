56d0dc248984bf1d1d3e0af49b12f8828ed18298
Tin <lychanhtintv@gmail.com>
2023-05-27 16:59:28 +0700

    create about app and update other setting screens

--- src/Screens/Setting/StartScreenScreen.tsx
+++ src/Screens/Setting/StartScreenScreen.tsx
@@ -2,67 +2,74 @@ import React, { useState } from 'react';
 import {
     StyleSheet,
     SafeAreaView,
-    ScrollView,
     View,
     Text,
     Image,
-    TouchableOpacity,
-    Switch,
-    useWindowDimensions,
-    Button,
-    Pressable
+    Pressable,
+    Alert
   } from 'react-native';
 
 export const StartScreenScreen = ({navigation}) =>  {
+    const [startScreen, setStartScreen] = useState([false, true, false])
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
       <SafeAreaView style={{backgroundColor: '#fff'}}>
         <View style={{marginHorizontal: 24, marginVertical: 16}}>
             <View style={{marginBottom: 24}}>
-                <Text style={styles.title}>Màn hình bắt đàu</Text>
+                <Text style={styles.title}>Màn hình bắt đầu</Text>
                 <Text style={styles.description}>Chọn màn hình xuất hiện khi mở ứng dụng</Text>
             </View>
 
             <View style={styles.wrapper}>
                 <View style={styles.startListWrapper}>
                     <View style={styles.startList}>
-                        <Pressable style={styles.startItem}>
-                            <Text style={{fontSize: 14, margin: 6, fontWeight: '400', color: '#9D9D9D'}}>Nhà riêng</Text>
+                        <Pressable
+                         style={styles.startItem}
+                         onPress={()=>{setStartScreen([true,false,false])}}>
+                            <Text style={[styles.label, startScreen[0]?styles.selectedLabel:styles.unSelectedLabel]}>Vé của tôi</Text>
                         </Pressable>
-                        <Pressable style={styles.startItem}>
-                            <Text style={{fontSize: 14, margin: 6, fontWeight: '600', color: '#1F1F1F'}}>Nơi làm việc</Text>
+                        <Pressable
+                         style={styles.startItem}
+                         onPress={()=>{setStartScreen([false,true,false])}}>
+                            <Text style={[styles.label, startScreen[1]?styles.selectedLabel:styles.unSelectedLabel]}>Tra cứu</Text>
                         </Pressable>
-                        <Pressable style={styles.startItem}>
-                            <Text style={{fontSize: 14, margin: 6, fontWeight: '400', color: '#9D9D9D'}}>Trường học</Text>
+                        <Pressable
+                         style={styles.startItem}
+                         onPress={()=>{setStartScreen([false,false,true])}}>
+                            <Text style={[styles.label, startScreen[2]?styles.selectedLabel:styles.unSelectedLabel]}>Trang của tôi</Text>
                         </Pressable>
                     </View>
                 </View>
                 <View style={styles.startScreenSample}>
-                    <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+                    <View style={[styles.startImageWrapper, startScreen[0]?{zIndex: 3}:{zIndex: 1}]}>
                         <Image
-                            style={[styles.startImage, {position: 'absolute', right: '50%', height: 240, width: 120}]}
+                            style={[styles.startImage, {right: '50%'}, startScreen[0]?styles.selectedScreen:styles.unSelectedScreen]}
                             source={require('../../../assets/start/ticket.png')}
                         />
                     </View>
-
-                    <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+                    <View style={[styles.startImageWrapper, startScreen[1]?{zIndex: 3}:{zIndex: 2}]}>
                         <Image
-                            style={[styles.startImage, {position: 'absolute', left: '50%', height: 240, width: 120}]}
+                            style={[styles.startImage, startScreen[1]?styles.selectedScreen:styles.unSelectedScreen]}
+                            source={require('../../../assets/start/initial.png')}
+                        />
+                    </View>
+
+                    <View style={[styles.startImageWrapper, startScreen[2]?{zIndex: 3}:{zIndex: 1}]}>
+                        <Image
+                            style={[styles.startImage, {left: '50%'}, startScreen[2]?styles.selectedScreen:styles.unSelectedScreen]}
                             source={require('../../../assets/start/mypage.png')}
                         />
                     </View>
 
-                    <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
-                        <Image
-                            style={[styles.startImage, {position: 'absolute',}]}
-                            source={require('../../../assets/start/initial.png')}
-                        />
-                    </View>
+
                 </View>
             </View>
 
-            <Pressable style={styles.saveBtn}>
+            <Pressable style={styles.saveBtn} onPress={() => Alert.alert(`BusGuide đã lưu màn hình bắt đầu của bạn`)}>
                 <Text style={{color: '#fff'}}>Lưu</Text>
             </Pressable>
+
         </View>
       </SafeAreaView>
     );
@@ -125,11 +132,42 @@ const styles = StyleSheet.create({
         height: 380,
         marginBottom: 12,
     },
+    startImageWrapper: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     startImage: {
         width: 140,
         height: 280,
         borderRadius: 16,
         borderWidth: 2,
         borderColor: '#000',
-    }
+        position: 'absolute',
+    },
+    selectedScreen: {
+    },
+    label: {
+        fontSize: 14,
+        margin: 6,
+
+    },
+    unSelectedLabel: {
+        fontWeight: '400',
+        color: '#9D9D9D',
+    },
+    selectedLabel: {
+        fontWeight: '600',
+        color: '#1F1F1F',
+    },
+    unSelectedScreen: {
+        height: 240,
+        width: 120,
+    },
+
+
 })
